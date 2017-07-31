6d0a1d4ec50e01a2499d9e687aebba364ae6d7ff
黄存璘 <hcl@meitu.com>
2017-07-31 17:47:11 +0800

    [代码更新] 添加develop 分支

--- proj/Mac/HBAcl/Acl_xcode/main.cpp
+++ proj/Mac/HBAcl/Acl_xcode/main.cpp
@@ -1,15 +1,12 @@
 //
 //  main.cpp
-//  HBAcl
+//  Acl_xcode
 //
 //  Created by zj-db0519 on 2017/7/31.
 //  Copyright © 2017年 meitu. All rights reserved.
 //
 
 #include <iostream>
-
-/** UDP 服务端程序 */
-
 
 int main(int argc, const char * argv[]) {
     // insert code here...
